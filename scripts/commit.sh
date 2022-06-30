c3fdef0e16a0da23ad14ba351e779e7ea38093f1
Roland Rabien <figbug@gmail.com>
2022-06-30 07:57:07 -0700

    More scripts

---
+++ scripts/commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ROOT=$(cd "$(dirname "$0")/.."; pwd)
+cd "$ROOT"
+echo "$ROOT"
+
+cd "$ROOT/scripts"
+cat plugins.txt | while read PLUGIN; do
+  PLUGIN=$(echo $PLUGIN|tr -d '\n\r ')
+
+  cd "$ROOT/modules/$PLUGIN"
+  git add -A
+  git commit --message "Enable LV2"
+  git push
+
+done
--- scripts/release.sh
+++ scripts/release.sh
@@ -8,21 +8,17 @@ cd "$ROOT/scripts"
 cat plugins.txt | while read PLUGIN; do
   PLUGIN=$(echo $PLUGIN|tr -d '\n\r ')
 
-  echo ""
-  echo "*** Updating JUCE for $PLUGIN***"
-  echo ""
-
   cd "$ROOT/modules/$PLUGIN"
   git reset --hard
   git checkout master
   git pull
+  git submodule update
 
-  cd "$ROOT/modules/$PLUGIN/modules/juce"
-  git reset --hard
-  git checkout develop
+  git checkout release
   git pull
-  cd ../..
-  git add -A
-  git commit --message "Update JUCE"
+  git merge master
   git push
+
+  git checkout master
+
 done
